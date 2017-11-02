73b8fbea11792ce4bb2e6e0ab25f46c0efc51504
dolare <dolarebrave@gmail.com>
2017-11-02 19:52:39 +0800

    add houtai entry router

--- src/router.js
+++ src/router.js
@@ -1,18 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Dashboard from './houtai/components/pages/Dashboard.vue'
-import QiantaiHome from './qiantai/components/pages/Home.vue'
+import AdminHome from './houtai/components/pages/Dashboard.vue'
 import Login from  './houtai/components/pages/Login.vue'
+
+import QiantaiHome from './qiantai/components/pages/Home.vue'
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
-            path: '/admin',
+            path: '/login',
             name: 'Login',
-            component: Login
+            component: Login,
+        },
+        {
+            path: '/admin',
+            name: 'AdminHome',
+            component: AdminHome,
         },
         {
             path: '/',
